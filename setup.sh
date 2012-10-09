cd008105716025674f44b62a76febd71715526c7
313 Dev <dev@dev-HP-ProBook-4710s.(none)>
2012-10-09 09:43:30 -0400

    more removal stuff

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 sudo apt-get -y upgrade
 sudo apt-get -y install gcc git curl
 
-sudo apt-get --purge remove ruby-rvm
+# Clear out any ubuntu RVM stuff
+sudo apt-get -y --purge remove ruby-rvm
+sudo apt-get -y autoremove
 sudo rm -rf /usr/share/ruby-rvm /etc/rvmrc /etc/profile.d/rvm.sh
 
+# install RVM and ruby 1.9.3
 \curl -L https://get.rvm.io | bash -s stable --ruby
-
 rvm install 1.9.3
 rvm --default use 1.9.3
